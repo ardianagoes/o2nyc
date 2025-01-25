03c043c7d267ac8e8ebbdd6277ce7ceee3ad1574
ardianagoes <ardianagoes83@gmail.com>
2025-01-25 09:48:57 -0500

    base home page created

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Home from "./pages/Home";
+
+const App = () => {
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <Box style={{ height: windowHeight }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </Box>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import "./Home.css";
+import { Box, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box className="Home-page">
+      <Typography>hi</Typography>
+    </Box>
+  );
+}
